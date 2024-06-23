53bcc7d6836e63f8adfac6c79b82b7d6678e0c71
xCHZx <chzx94@gmail.com>
2024-06-22 20:37:26 -0500

    Fixed Paused ubscription view in Planes page

--- src/pages/empresa/planes/index.vue
+++ src/pages/empresa/planes/index.vue
@@ -57,7 +57,66 @@ onMounted(() => {
           class="mx-2"
           icon="tabler-mail"
         />
-        <span class="text-primary">soporte@repitt.com</span>
+        <span class="text-primary">
+          soporte@repitt.com
+        </span>
+      </div>
+      <div class="text-center text-h6 mt-0">
+        También nos puedes contactar por WhatsApp
+        <VIcon
+          class="mx-2"
+          icon="tabler-brand-whatsapp"
+        />
+        <span class="text-primary">
+          +52 983 106 9602
+        </span>
+      </div>
+    </VCardText>
+  </div>
+  <div v-else-if="authStore.subscriptionStatus === 'paused'">
+    <VCardText>
+      <div class="text-center text-h2 font-weight-bold">
+        ¡Tu suscripción está pausada!
+      </div>
+    </VCardText>
+    <VCardText>
+      <div class="text-center text-h5">
+        <strong>Es necesario agregar un método de pago para seguir disfrutando de los beneficios de Repitt</strong>
+        <br>
+        Accede a tu portal de administración de suscripciones aquí
+      </div>
+      <div class="text-center mt-4">
+        <VBtn
+          block
+          color="primary"
+          @click="goToBillingPortal"
+        >
+          Ir al panel
+          <VIcon
+            end
+            icon="tabler-credit-card"
+          />
+        </VBtn>
+      </div>
+      <div class="text-center text-h6 mt-6">
+        Si necesitas ayuda, por favor contáctanos al correo
+        <VIcon
+          class="mx-2"
+          icon="tabler-mail"
+        />
+        <span class="text-primary">
+          soporte@repitt.com
+        </span>
+      </div>
+      <div class="text-center text-h6 mt-0">
+        También nos puedes contactar por WhatsApp
+        <VIcon
+          class="mx-2"
+          icon="tabler-brand-whatsapp"
+        />
+        <span class="text-primary">
+          +52 983 106 9602
+        </span>
       </div>
     </VCardText>
   </div>
